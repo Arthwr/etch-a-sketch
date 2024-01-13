527f757c1834dd5c30bca6263d4b5496fe980fca
arthur <artyr007@gmail.com>
2024-01-13 22:40:55 +0500

    [ADD] shading mode

--- scripts.js
+++ scripts.js
@@ -26,18 +26,37 @@ function createGrid(gridSize) {
 // Drawing
 isMouseDown = false;
 isRainbowModeOn = false;
+isShadingModeOne = false;
 
-const randomRGB = function () {
-  let red = Math.floor(Math.random() * 256);
-  let green = Math.floor(Math.random() * 256);
-  let blue = Math.floor(Math.random() * 256);
+const getRandomRGB = function () {
+  const randomColorComponent = () => Math.floor(Math.random() * 256);
+  const red = randomColorComponent();
+  const green = randomColorComponent();
+  const blue = randomColorComponent();
   return `rgb(${red}, ${green}, ${blue})`;
+};
+
+const getDarkerRGB = function (currentRed, currentGreen, currentBlue) {
+  const darkenComponent = (component) => Math.max(0, component * 0.9);
+  let darkerRed = darkenComponent(currentRed);
+  let darkerGreen = darkenComponent(currentGreen);
+  let darkerBlue = darkenComponent(currentBlue);
+
+  return `rgb(${darkerRed}, ${darkerGreen}, ${darkerBlue})`;
 };
 
 function drawSquare(event) {
   if (event.target.className === "square" && isMouseDown) {
     if (isRainbowModeOn) {
-      event.target.style.backgroundColor = randomRGB();
+      event.target.style.backgroundColor = getRandomRGB();
+    } else if (isShadingModeOne) {
+      const computedStyle = getComputedStyle(event.target);
+      const colorString = computedStyle.backgroundColor;
+      const [, red, green, blue] = colorString
+        .match(/(\d+)\s*,\s*(\d+)\s*,\s*(\d+)/)
+        .map(Number);
+      event.target.style.backgroundColor = getDarkerRGB(red, green, blue);
+      return;
     } else {
       event.target.style.backgroundColor = "black";
     }
@@ -54,6 +73,7 @@ container.addEventListener("mouseover", drawSquare);
 // UI
 const sizeButton = document.querySelector("#sizeButton");
 const rainbowButton = document.querySelector("#rainbowButton");
+const shadingButton = document.querySelector("#shadingButton");
 
 function changeSize() {
   const gridSizeInput = prompt("Change a grid size (not more than 100x100)");
@@ -67,6 +87,7 @@ function changeSize() {
 
 sizeButton.addEventListener("click", changeSize);
 rainbowButton.addEventListener("click", toggleRainbowMode);
+shadingButton.addEventListener("click", toggleShadingMode);
 
 // Helper functions
 function removeAllChildNodes(parent) {
@@ -78,7 +99,19 @@ function removeAllChildNodes(parent) {
 function toggleRainbowMode() {
   rainbowButton.classList.toggle("active");
   isRainbowModeOn = !isRainbowModeOn;
-  
+  if (isShadingModeOne) {
+    shadingButton.classList.remove("active");
+    isShadingModeOne = false;
+  }
+}
+
+function toggleShadingMode() {
+  shadingButton.classList.toggle("active");
+  isShadingModeOne = !isShadingModeOne;
+  if (isRainbowModeOn) {
+    rainbowButton.classList.remove("active");
+    isRainbowModeOn = false;
+  }
 }
 
 // Init
